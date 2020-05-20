9c474d2b90814e2b7a0bfc12064053649b395962
Rachel Haub <rachelhaub@gmail.com>
2020-05-19 17:13:24 -0700

    changed some file names, added character material

---
+++ IKPlatforming/Assets/Scripts/FootIKControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootIKControl : MonoBehaviour
+{
+    protected Animator animator;
+
+    public bool ikActive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
